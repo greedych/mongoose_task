a904782365d5e7dfbda5a6359d5fdd66134d9cdc
HydE <chucha1204@gmail.com>
2026-03-19 20:41:37 +0100

    setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import mongoose from "mongoose";
+
+configDotenv();
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+const HOST = process.env.HOST || "http://localhost";
+const dbURI = process.env.MONGO_URI || "URI";
+
+app.use(express.json());
+
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Faild connected to MongoDB");
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server running at ${HOST}:${PORT}`);
+});
